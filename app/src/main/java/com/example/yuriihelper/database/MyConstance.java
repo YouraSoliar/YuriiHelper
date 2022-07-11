a3884441998cfdc2ac8b2492b650db8029b51a57
Android <us1585707@gmail.com>
2022-07-11 17:03:46 +0300

    constant class v1

---
+++ app/src/main/java/com/example/yuriihelper/database/MyConstance.java
@@ -0,0 +1,14 @@
+package com.example.yuriihelper.database;
+
+public class MyConstance {
+    public static final String TABLE_NAME = "table_car";
+    public static final String _ID = "_id";
+    public static final String NAME = "name";
+    public static final String PRICE = "price";
+    public static final String CHANGE_DATE = "change_date";
+    public static final String CHANGE_KM = "change_km";
+    public static final String RECOMMENDED_DATE = "recommended_date";
+    public static final String RECOMMENDED_KM = "recommended_km";
+    public static final String DB_NAME = "db_car.db";
+    public static final int VERSION = 1;
+}
